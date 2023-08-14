35cf4f180b5049f439a3756615be55791e0264dd
dnierman0920 <40249436+dnierman0920@users.noreply.github.com>
2023-08-14 12:24:49 -0400

    Update Garbage Collection Demo to Help Chart

--- GCDemo/GarbageColectionDemo.cs
+++ GCDemo/GarbageColectionDemo.cs
@@ -1,6 +1,9 @@
 ﻿
 
 
+using System.Diagnostics;
+using Xunit.Abstractions;
+
 namespace GarbageCollectionDemo
 {
     public class GarbageCollectionDemoTests
@@ -13,37 +16,41 @@ namespace GarbageCollectionDemo
         {
             using StreamWriter sw = new("../../../gcdemo.csv");
             sw.Flush();
-            memoryAllocator.AllocateMemoryLoop(sw, 50000);
+            memoryAllocator.AllocateMemoryLoop(sw, 200000);
         }
 
     }
-
 
 }
 class MemoryAllocator
 {
     public void AllocateMemoryLoop(TextWriter sw, int iterations)
     {
+        sw.WriteLine($"memoryAddress,memoryUsage, garbageCollectionCount ");
+
         unsafe
         {
+            long firstAddress = 0;
+            GC.Collect();
             for (int i = 0; i < iterations; i++)
             {
                 var o = new Foo();
+
                 fixed (void* p = &o._a)
                 {
-
+                    IntPtr ptr = (IntPtr)p;
+                    if (i == 0)
                     {
-                        IntPtr ptr = (IntPtr)p;
-                        sw.WriteLine($"{ptr},");
+                        firstAddress = ptr.ToInt64(); // adding this hear to make the address number small enough for google sheets to chart
+
                     }
+                    sw.WriteLine(value: $"{ptr.ToInt64() - firstAddress}, {GC.GetTotalMemory(false)}, {GC.CollectionCount(0) * 100000} "); // multiplying GC count so we can display it on a chart and actually see it on google sheets
                 }
             }
         }
     }
 
 }
-
-
 
 class Foo
 {

99514fd11836c2885691636d67166f6ef7f24fad
dnierman0920 <40249436+dnierman0920@users.noreply.github.com>
2023-08-14 08:41:30 -0400

    refactor garbage collection demo into a test

---
+++ GCDemo/GarbageColectionDemo.cs
@@ -0,0 +1,51 @@
+﻿
+
+
+namespace GarbageCollectionDemo
+{
+    public class GarbageCollectionDemoTests
+    {
+
+        readonly MemoryAllocator memoryAllocator = new();
+
+        [Fact]
+        public void RunMemoryAllocator()
+        {
+            using StreamWriter sw = new("../../../gcdemo.csv");
+            sw.Flush();
+            memoryAllocator.AllocateMemoryLoop(sw, 50000);
+        }
+
+    }
+
+
+}
+class MemoryAllocator
+{
+    public void AllocateMemoryLoop(TextWriter sw, int iterations)
+    {
+        unsafe
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                var o = new Foo();
+                fixed (void* p = &o._a)
+                {
+
+                    {
+                        IntPtr ptr = (IntPtr)p;
+                        sw.WriteLine($"{ptr},");
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+
+
+class Foo
+{
+    public long _a;
+}

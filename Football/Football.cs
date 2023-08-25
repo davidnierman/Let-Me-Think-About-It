ca4302649fc907a3041a9d6cb9d2cd30692357e8
dnierman0920 <40249436+dnierman0920@users.noreply.github.com>
2023-08-25 10:21:13 -0400

    added game points to season & theory to test different seasons

--- Football/Football.cs
+++ Football/Football.cs
@@ -1,11 +1,20 @@
 
 
 using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 
 namespace FootballTests
 {
     public class FootballTests
     {
+
+        // you have a set of teams identified by name (Chargers, Rams, Seahawks, 49ers, Raiders, Cardinals, Bronocs)
+        public static IEnumerable<object[]> Teams => new object[][]
+   {
+        new object[] { new Team[] { new Team("Chargers"), new Team("Rams") } },
+        new object[] { new Team[] { new Team("Rams"), new Team("Chargers") } },
+        new object[] { new Team[] { new Team("Rams"), new Team("Chargers"), new Team("SeaHawks") } }
+   };
 
 
         [Fact]
@@ -30,8 +39,12 @@ namespace FootballTests
             Assert.Equal(true, game.Final);
             Assert.NotNull(game.HomeTeamPoints);
             Assert.NotNull(game.AwayTeamPoints);
+            Assert.Throws<Season.Game.GameAlreadyPlayed>(game.Play);
         }
 
+        //  home wins are worth 2 season tallies
+        // away wins are worth 3 season tallies
+        // draws are always worth 1 season tallies
         [Fact]
         public void TestAddingSeasonPoints()
         {
@@ -42,44 +55,29 @@ namespace FootballTests
             Season.Game game = new(chargers, rams);
             game.Play();
             mmxxiii.AddSeasonPoints(game);
-            // using game points to verify --> easier to test when data has accessors/getters
-            Assert.Equal(game.AwayTeamPoints > game.HomeTeamPoints, mmxxiii.TallyTable[rams] - 3 == mmxxiii.TallyTable[chargers]);
-            Assert.Equal(game.AwayTeamPoints < game.HomeTeamPoints, mmxxiii.TallyTable[rams] + 2 == mmxxiii.TallyTable[chargers]);
+            Assert.Equal(game.AwayTeamPoints > game.HomeTeamPoints, mmxxiii.TallyTable[rams].Talies - 3 == mmxxiii.TallyTable[chargers].Talies); // learned probability kata
+            Assert.Equal(game.AwayTeamPoints < game.HomeTeamPoints, mmxxiii.TallyTable[rams].Talies + 2 == mmxxiii.TallyTable[chargers].Talies); // learned probability kata
             Assert.Equal(game.AwayTeamPoints == game.HomeTeamPoints, mmxxiii.TallyTable[rams] == mmxxiii.TallyTable[chargers]);
+            Assert.Equal(mmxxiii.TallyTable[chargers].Points, game.HomeTeamPoints - game.AwayTeamPoints);
+            Assert.Equal(mmxxiii.TallyTable[rams].Points, game.AwayTeamPoints - game.HomeTeamPoints);
         }
 
-        [Fact]
-        public void CannotPlayMoreThanOneGame()
+        // each season every team plays one game at home and one game away
+        [Theory]
+        [MemberData(nameof(Teams))]
+        public void TestCreateSeasonGames(Team[] teams)
         {
-            Team chargers = new Team("Chargers");
-            Team rams = new Team("Rams");
-            Team[] teams = { chargers, rams };
-            Season mmxxiii = new Season(teams);
-            Season.Game game = new(chargers, rams);
-            game.Play();
-            Assert.Throws<Season.Game.GameAlreadyPlayed>(game.Play);
-        }
-        [Fact]
-        public void PlaySeason()
-        {
-            Team chargers = new Team("Chargers");
-            Team rams = new Team("Rams");
-            Team[] teams = { chargers, rams };
-            Season mmxxiii = new Season(teams);
-            Season.Game game = new(chargers, rams);
-            game.Play();
-            Assert.Throws<Season.Game.GameAlreadyPlayed>(game.Play);
-        }
-
-        [Fact]
-        public void TestCreateSeasonGames()
-        {
-            Team chargers = new Team("Chargers");
-            Team rams = new Team("Rams");
-            Team[] teams = { chargers, rams };
             Season mmxxiii = new Season(teams);
             var seasonGames = mmxxiii.CreateSeasonGames();
             Assert.Equal(teams.Length, seasonGames.Count);
+        }
+
+        // rank in order of most season tallies
+        // in the event of a tie with season tallies the winner is determined by the difference in points scored by them vs the points scored against them
+        [Fact]
+        public void TestRankTeams()
+        {
+
         }
 
     }
@@ -87,16 +85,13 @@ namespace FootballTests
     //  PRODUCTION CODE BELOW THIS LINE
     // *********************************
 
-    /* you have a set of teams identified by name (Chargers, Rams, Seahawks, 49ers, Raiders, Cardinals, Bronocs)
-home wins are worth 2 season points
-away wins are worth 3 season points
-draws are always worth 1 season point
-each season every team plays one game at home and one game away
-in the event of a tie the winner is determined by the difference in points scored by them vs the points scored against them
-the league table should be available at any point in time with those who have played the most games ranked above those that have played less
-bonus points for keeping data private*/
+    /* 
+        in the event of a tie the winner is determined by the difference in points scored by them vs the points scored against them
+        the league table should be available at any point in time with those who have played the most games ranked above those that have played less
+        bonus points for keeping data private
+    */
 
-    class Team
+    public class Team
     {
         private string _name;
 
@@ -113,22 +108,22 @@ bonus points for keeping data private*/
 
     class Season
     {
-        private Dictionary<Team, int> _tallyTable = new Dictionary<Team, int>();
+        private Dictionary<Team, TeamRecord> _tallyTable = new Dictionary<Team, TeamRecord>();
 
-        public Dictionary<Team, int> TallyTable => _tallyTable;
+        public Dictionary<Team, TeamRecord> TallyTable => _tallyTable;
 
         public Season(Team[] teams)
         {
             foreach (var team in teams)
             {
-                _tallyTable.Add(team, 0);
+                _tallyTable.Add(team, new TeamRecord());
             }
         }
 
         public HashSet<Game> CreateSeasonGames()
         {
             HashSet<Game> SeasonGames = new();
-            List<Team> teamList = _tallyTable.Keys.ToList<Team>();
+            List<Team> teamList = _tallyTable.Keys.ToList();
             Random rnd = new Random();
 
             HashSet<int> awayGames = new();
@@ -141,14 +136,11 @@ bonus points for keeping data private*/
                 }
                 else
                 {
+                    Game game = new Game(teamList[i], teamList[randomIndex]);
+                    SeasonGames.Add(game);
                     awayGames.Add(randomIndex);
                     i++;
                 }
-            }
-            for (int i = 0; i < teamList.Count; i++)
-            {
-                Game game = new Game(teamList[i], teamList[awayGames.ElementAt(i)]);
-                SeasonGames.Add(game);
             }
             return SeasonGames;
         }
@@ -163,19 +155,26 @@ bonus points for keeping data private*/
         // I want the below method private, but cannot test if it is...
         public void AddSeasonPoints(Game game)
         {
+            var homeTeamRecord = _tallyTable[game.HomeTeam];
+            var awayTeamRecord = _tallyTable[game.AwayTeam];
+
+            homeTeamRecord.Points = game.HomeTeamPoints - game.AwayTeamPoints;
+            awayTeamRecord.Points = game.AwayTeamPoints - game.HomeTeamPoints;
+
             if (game.HomeTeamPoints > game.AwayTeamPoints)
             {
-                _tallyTable[game.HomeTeam] += 2;
+                homeTeamRecord.Talies += 2;
             }
             else if (game.AwayTeamPoints > game.HomeTeamPoints)
             {
-                _tallyTable[game.AwayTeam] += 3;
+                awayTeamRecord.Talies += 3;
             }
             else
             {
-                _tallyTable[game.HomeTeam] += 1;
-                _tallyTable[game.AwayTeam] += 1;
+                homeTeamRecord.Talies += 1;
+                awayTeamRecord.Talies += 1;
             }
+
         }
 
 
@@ -224,16 +223,24 @@ bonus points for keeping data private*/
                 }
 
             }
+        }
+        public class TeamRecord
+        {
 
-
-
+            public int Talies;
+            public int Points;
 
         }
 
+
+
+
     }
 
-
 }
+
+
+
 
 
 

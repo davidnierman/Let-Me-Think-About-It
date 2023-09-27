30adceb3399e59376b245e8d6315ce9266d2914b
David Nierman <david.nierman@solliance.net>
2023-09-27 14:06:38 -0400

    Add Urgent Messaging & subscribing to message types

--- PingPong/PingPong.cs
+++ PingPong/PingPong.cs
@@ -36,12 +36,35 @@ public class PingPongTests
         var sink = new Sink();
         var machine1 = new Machine(ping, sink, _logger);
         var machine2 = new Machine(pong, sink, _logger);
-        Assert.True(sink.Subscribe(machine1));
-        Assert.True(sink.Subscribe(machine2));
-        machine1.SendMessage(sink);
+        Assert.True(sink.SubscribeToAll(machine1));
+        Assert.True(sink.SubscribeToAll(machine2));
+        machine1.SendMessageToEveryone(sink);
+        machine2.SendMessageToEveryone(sink);
+    }
+
+    [Fact]
+    public void SubscribeToMessageTypes()
+    {
+        var ping = new Message("ping");
+        var pong = new Message("pong");
+        var emergencyPing = new UrgentMessage("ping!!!!");
+        var sink = new Sink();
+        var machine1 = new Machine(ping, sink, _logger);
+        var machine2 = new Machine(pong, sink, _logger);
+        sink.SubscribeToMessageType(machine2, emergencyPing);
+        sink.SubscribeToMessageType(machine1, emergencyPing);
+        machine1.SendMessageToMessageSubscribers(sink, emergencyPing);
     }
 }
 
+public class UrgentMessage : Message
+{
+
+    public UrgentMessage(string m) : base(m)
+    {
+
+    }
+}
 
 public interface ILogger
 {
@@ -49,7 +72,17 @@ public interface ILogger
 }
 
 
-public record Message(string M);
+public class Message
+{
+    private string _m;
+
+    public string M => _m;
+
+    public Message(string m)
+    {
+        _m = m;
+    }
+}
 
 public class Machine
 {
@@ -65,15 +98,28 @@ public class Machine
         _logger = logger;
     }
 
-    public void SendMessage(Sink sink)
+    public void SendMessageToEveryone(Sink sink)
     {
         sink.Broadcast(_defaultMessage, this);
+    }
+
+    public void SendMessageToMessageSubscribers(Sink sink, Message message)
+    {
+        sink.Route(message, this);
     }
 
     public void ReceiveMessage(Message message)
     {
         _logger.WriteLine($"{_defaultMessage.M} received {message.M}");
-        SendMessage(_sink);
+        if (message is UrgentMessage)
+        {
+            SendMessageToMessageSubscribers(_sink, message);
+        }
+        else
+        {
+            SendMessageToEveryone(_sink);
+        }
+
     }
 }
 
@@ -84,14 +130,16 @@ public class Sink
 
     private readonly List<Machine> _machines;
 
+    private readonly Dictionary<Type, List<Machine>> _messageSubcriptions;
+
     public Sink()
     {
         _machines = new();
+        _messageSubcriptions = new();
     }
 
     public void Broadcast(Message message, Machine sender)
     {
-        Console.WriteLine(_counter);
         _counter++;
         if (_counter > 10)
         {
@@ -107,12 +155,43 @@ public class Sink
         }
     }
 
-    public bool Subscribe(Machine machine)
+    public void Route(Message message, Machine sender)
+    {
+        _counter++;
+        if (_counter > 3)
+        {
+            return;
+        }
+        foreach (Machine machine in _messageSubcriptions[message.GetType()])
+        {
+            //if (machine == sender)
+            //{
+            //  continue;
+            //}
+            machine.ReceiveMessage(message);
+        }
+
+    }
+
+    public bool SubscribeToAll(Machine machine)
     {
         _machines.Add(machine);
         return _machines.Contains(machine);
     }
+    public bool SubscribeToMessageType(Machine machine, Message message)
+    {
 
+        if (_messageSubcriptions.ContainsKey(message.GetType()))
+        {
+            var subsribedMachines = _messageSubcriptions[message.GetType()];
+            subsribedMachines.Add(machine);
+        }
+        else
+        {
+            _messageSubcriptions.Add(message.GetType(), new List<Machine> { machine });
+        }
+        return _messageSubcriptions[message.GetType()].Contains(machine);
+    }
 
 }
 
